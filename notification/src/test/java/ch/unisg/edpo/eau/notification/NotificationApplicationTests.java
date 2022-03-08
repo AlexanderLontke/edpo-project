635e3819121c1bb69ed529f13be240ad37904e8e
Alexander Lontke <77338841+AlexanderLontke@users.noreply.github.com>
2022-03-08 09:49:03 +0100

    Init commit

---
+++ notification/src/test/java/ch/unisg/edpo/eau/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg.edpo.eau.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

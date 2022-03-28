8b1052215583ee2fdc169d7457a4d69da0d30e68
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2022-03-28 11:28:18 +0200

    Added onboarding service

---
+++ onboarding/src/main/java/ch/unisg/edpo/eau/onboarding/common/ConfigProperties.java
@@ -0,0 +1,17 @@
+package ch.unisg.edpo.eau.onboarding.common;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ConfigProperties {
+    @Autowired
+    private Environment environment;
+
+    public String getExampleVariable() {
+        return Objects.requireNonNull(environment.getProperty("example"));
+    }
+
+}
